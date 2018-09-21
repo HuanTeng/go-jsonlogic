ad069c7e7d2943850e135534b04795740c044e73
tianji <tj@hhttech.com>
2018-09-21 17:17:20 +0800

    add example test

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # go-jsonlogic
 Golang implementation of JsonLogic (jsonlogic.com)
+
+## Example
+
+```golang
+var rule, data interface{}
+
+json.Unmarshal([]byte(`
+{"if": [
+	{"<": [{"var":"temp"}, 0] }, "freezing",
+	{"<": [{"var":"temp"}, 100] }, "liquid",
+	"gas"
+]}
+`), &rule)
+
+json.Unmarshal([]byte(`{"temp":55}`), &data)
+
+got, err := jsonlogic.Apply(rule, data)
+if err != nil {
+	// handle error
+}
+
+fmt.Println(got)
+// Output: liquid
+
+```
---
+++ example_test.go
@@ -0,0 +1,29 @@
+package jsonlogic_test
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/HuanTeng/go-jsonlogic"
+)
+
+func ExampleApply() {
+	var rule, data interface{}
+
+	json.Unmarshal([]byte(`
+{"if" : [
+	{"<": [{"var":"temp"}, 0] }, "freezing",
+	{"<": [{"var":"temp"}, 100] }, "liquid",
+	"gas"
+]}`), &rule)
+
+	json.Unmarshal([]byte(`{"temp":55}`), &data)
+
+	got, err := jsonlogic.Apply(rule, data)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(got)
+	// Output: liquid
+}
